6cb02aa1c1b27bb9ab5f29bf3b7a7d47cc2250e3
Aniesodo collins <aniesodoc@gmail.com>
2025-05-10 14:53:21 +0100

    7commit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import authRoute from "./routes/auth.route.js";
 import cors from "cors";
+import { Server } from "socket.io";
+
+import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
-import { Server } from "socket.io";
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
